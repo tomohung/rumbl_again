0bb71fb2859465020aba7cb19e3153813c844765
Tomo Hung <tomohung@gmail.com>
2016-08-18 22:12:41 +0800

    add slug for video, use elixir protocls for video primary key

---
+++ priv/repo/migrations/20160818134738_add_slug_to_video.exs
@@ -0,0 +1,9 @@
+defmodule RumblAgain.Repo.Migrations.AddSlugToVideo do
+  use Ecto.Migration
+
+  def change do
+    alter table(:videos) do
+      add :slug, :string
+    end
+  end
+end
--- web/models/video.ex
+++ web/models/video.ex
@@ -1,10 +1,13 @@
 defmodule RumblAgain.Video do
   use RumblAgain.Web, :model
 
+  @primary_key {:id, RumblAgain.Permalink, autogenerate: true}
+
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, RumblAgain.User
     belongs_to :category, RumblAgain.Category
 
@@ -18,6 +21,27 @@ defmodule RumblAgain.Video do
     struct
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  defimpl Phoenix.Param, for: RumblAgain.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
   end
 end
