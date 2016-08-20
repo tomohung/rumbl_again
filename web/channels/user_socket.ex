0aefb78cf26071cc432c71fffb1b2c335af76c7d
Tomo Hung <tomohung@gmail.com>
2016-08-20 22:36:45 +0800

    add token verify for socket

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -8,9 +8,18 @@ defmodule RumblAgain.UserSocket do
   transport :websocket, Phoenix.Transports.WebSocket
   # transport :longpoll, Phoenix.Transports.LongPoll
 
-  def connect(_params, socket) do
-    {:ok, socket}
+  @max_age 2*7*24*60*60
+
+  def connect(%{"token" => token}, socket) do
+    case Phoenix.Token.verify(socket, "user socket", token, max_age: @max_age) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} ->
+        :error
+    end
   end
 
-  def id(_socket), do: nil
+  def connect(_params, _socket), do: :error
+
+  def id(socket), do: "users_socket:#{socket.assigns.user_id}"
 end
